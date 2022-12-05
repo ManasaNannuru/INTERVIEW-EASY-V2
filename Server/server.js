c7ce1c18e916bf689cc5cd0ac7ef921466c2595b
SravyaVaddi <svaddi@albany.edu>
2022-12-04 19:42:29 -0500

    Uploading resume to s3  buckets

--- Server/server.js
+++ Server/server.js
@@ -1,8 +1,11 @@
 const app = require("express")();
 const server = require("http").createServer(app);
+require("dotenv").config();
 const cors = require("cors");
 const formidable = require("formidable");
 const fs = require("fs");
+const multer = require("multer");
+const AWS = require("aws-sdk");
 const config = require("./config");
 const { ExpressPeerServer } = require("peer");
 const path = require("path");
@@ -37,7 +40,82 @@ app.post("/", (req, res) => {
   res.send("server is running");
 });
 
-app.post("/upload", (req, res) => {
+//file upload and get part
+const storage = multer.diskStorage({
+  destination: "uploads/",
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_S3_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_S3_SECRET_KEY,
+  region: process.env.AWS_S3_REGION,
+});
+
+app.post("/upload", upload.single("file"), (req, res) => {
+  console.log(req.file);
+  const userID = req.header("user-id");
+  /* if (req.file == null) {
+    return res.status(400).json({ message: "Please choose the file" });
+  } */
+
+  uploadFile(req.file.path, req.file.filename, userID, res);
+});
+
+app.get("/resume/:userID", (req, res) => {
+  const userID = req.params["userID"];
+  console.log(userID);
+  const filename = `uploads/${userID}/Resume.pdf`;
+  retrieveFile(filename, res);
+});
+
+function uploadFile(source, targetName, userID, res) {
+  console.log("preparing to upload...");
+  fs.readFile(source, (err, filedata) => {
+    if (!err) {
+      const putParams = {
+        Bucket: process.env.AWS_S3_BUCKET,
+        Key: `uploads/${userID}/Resume.pdf`,
+        Body: filedata,
+      };
+      s3.putObject(putParams, (err, data) => {
+        if (err) {
+          console.log("Could nor upload the file. Error :", err);
+          return res.send({ success: false });
+        } else {
+          fs.unlink(source, (err) => {
+            if (err) console.log("Erorr:", err);
+          }); // Deleting the file from uploads folder(Optional).Do Whatever you prefer.
+          console.log("Successfully uploaded the file");
+          return res.send({ success: true });
+        }
+      });
+    } else {
+      console.log({ err: err });
+    }
+  });
+}
+
+function retrieveFile(filename, res) {
+  const getParams = {
+    Bucket: process.env.AWS_S3_BUCKET,
+    Key: filename,
+  };
+
+  s3.getObject(getParams, function (err, data) {
+    if (err) {
+      return res.status(400).send({ success: false, err: err });
+    } else {
+      return res.send(data.Body);
+    }
+  });
+}
+
+/* app.post("/upload", (req, res) => {
   let form = new formidable.IncomingForm({
     uploadDir: path.join(__dirname, config.default.vault),
     keepExtensions: true,
@@ -59,65 +137,58 @@ app.post("/upload", (req, res) => {
       res.end();
     });
   });
-});
+}); */
 
-app.get("/resume/:userID", (req, res) => {
+/* app.get("/resume/:userID", (req, res) => {
   let filePath = path.join(
     __dirname,
     config.default.vault,
     req.param("userID"),
     "Resume"
   );
-
+  // if (fs.(filePath)) {
   var file = fs.createReadStream(filePath);
   var stat = fs.statSync(filePath);
   res.setHeader("Content-Length", stat.size);
   res.setHeader("Content-Type", "application/pdf");
   res.setHeader("Content-Disposition", "attachment; filename=Resume.pdf");
   file.pipe(res);
-});
+  // }
+}); */
 
-const exportAll = (userListByRoomID, messages) => {
+const exportAll = (userListByRoomID, messagesByRoomID) => {
   let interviewer = {};
   let interviewee = {};
   for (const key in userListByRoomID) {
-    const user = userListByRoomID[key];
+    const user = userListByRoomID[user];
     if (user.isInterviewer) interviewer = user;
     else interviewee = user;
   }
 
   const sender = nodemailer.createTransport({
-    host: "smtp.office365.com",
-    port: "587",
-    secure: false,
+    service: "gmail",
     auth: {
-      user: "intervieweazy@outlook.com",
+      user: "noreplyintervieweasy@gmail.com",
       pass: "JustF0rFun",
     },
   });
 
-  const resumeAttachment = {
-    filename: "Resume.pdf",
-    path: path.join(__dirname, config.default.vault, interviewee.uid, "Resume"),
-  };
-
-  let messageContent = "";
-
-  messages.forEach((messageObj) => {
-    messageContent += `${messageObj.userName}: ${messageObj.message}
-    `;
-  });
-
-  const chatAttachment = {
-    filename: "chat.txt",
-    content: messageContent,
-  };
-
   var mail = {
-    from: "intervieweazy@outlook.com",
+    from: "no-reply-interview-easy@gmail.com",
     to: interviewer.email,
     subject: `Interview with ${interviewee.userName}`,
-    attachments: [resumeAttachment, chatAttachment],
+    attachments: [
+      {
+        filename: "Resume.pdf",
+        path: path.join(
+          __dirname,
+          config.default.vault,
+          intervieweeUID,
+          "Resume"
+        ),
+        cid: "Resume.pdf",
+      },
+    ],
     text: "Attached the details of the interview",
   };
 
@@ -152,7 +223,7 @@ io.on("connection", (socket) => {
     socket.on("disconnect-user", () => {
       socket.to(roomId).emit("user-disconnected", userInfo);
       socket.to(roomId).emit("on-screen-sharing", false);
-      exportAll(userListByRoomID[roomId], messagesByRoomID[roomId]);
+      // exportAll(userListByRoomID[roomId], messagesByRoomID[roomId]);
       delete userListByRoomID[roomId][`${userInfo.userName}${userInfo.email}`];
       io.in(roomId).emit("list-of-users", userListByRoomID[roomId]);
     });
@@ -171,7 +242,7 @@ io.on("connection", (socket) => {
     });
 
     socket.on("on-screen-sharing", (roomID, status) => {
-      socket.to(roomID).emit("on-screen-sharing", status);
+      socket.to(roomId).emit("on-screen-sharing", status);
     });
   });
 });
